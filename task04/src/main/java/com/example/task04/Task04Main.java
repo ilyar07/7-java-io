dc2ce86a9f3f5b5b970af9bfbb64abe74c251fe5
Илья <irotgan777@mail.ru>
2026-09-25 17:55:01 +0500

    task 4

--- task04/src/main/java/com/example/task04/Task04Main.java
+++ task04/src/main/java/com/example/task04/Task04Main.java
@@ -1,14 +1,20 @@
 package com.example.task04;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Task04Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл src/test/resources/input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - запустить программу
-        // - проверить, что получилось 351.731900
-
-        System.out.println("0.0");
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        while (scanner.hasNext()) {
+            String token = scanner.next();
+            try {
+                sum += Double.parseDouble(token);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        System.out.printf(Locale.US, "%.6f%n", sum);
     }
 }
